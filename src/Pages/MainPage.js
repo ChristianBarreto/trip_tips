8140c668bc5703b3a00789f451cb042f5db1df0e
ChristianBarreto <34282983+ChristianBarreto@users.noreply.github.com>
2021-11-04 10:17:14 -0300

    remove ugly code

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -11,36 +11,6 @@ function MainPage(props) {
   const [filter, setFilter] = useState('');
   const [product, setProduct] = useState('');
 
-  function stringsConstruction(a, b) {
-      const counts_b = {};
-      
-      for (var i in b){
-          if (counts_b.hasOwnProperty(b[i])){
-            counts_b[b[i]] +=1;
-          }else {
-            counts_b[b[i]] = 1;
-          }
-      }
-      
-      var values = Object.keys(counts_b).map(key => {
-        return counts_b[key];
-      });
-      console.log(values);
-
-      console.log(Math.min.apply(Math, values));
-
-      if (a.)
-      if (a.length == 1){
-        return Math.max.apply(Math, values)
-      }else {
-        return Math.min.apply(Math, values)
-      }
-
-  }
-
-  stringsConstruction("a", "b")
-
-
   return (
     <div className="app">
         <Header/>
